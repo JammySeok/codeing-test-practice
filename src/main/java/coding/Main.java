11048fa3ebb1b3abdc2184f56337081b4daefdbe
JammySeok <jjh326326@naver.com>
2026-05-21 15:19:12 +0900

    필요 없는 문제 삭제, 다시 풀어볼 문제 정리

--- src/main/java/coding/Main.java
+++ src/main/java/coding/Main.java
@@ -1,17 +1,21 @@
 package main.java.coding;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        /**
+         * [1단계] 다시 풀어볼 문제
+         * - 실패율
+         * - 키패드 누르기
+         * - 둘만의 암호
+         * - 공원 산책
+         * - 지폐 접기
+         * - 노란불 신호등
+         */
+
+         /**
+          * [2단계] 다시 풀어볼 문제
+          * - JadenCase 문자열만들기
+         */
     }
 }
